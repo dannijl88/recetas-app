351306eb3b0a7c5dc2f3821c9d80e43e36a3fe8e
Daniel Juan <dannijl88web@gmail.com>
2026-03-05 22:02:52 +0100

    Add Recipe entity

---
+++ src/main/java/com/dani/recetas/model/Recipe.java
@@ -0,0 +1,30 @@
+package com.dani.recetas.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name = "recipes")
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String ingredients;
+
+    @Column(name = "preparation_time", nullable = false)
+    private String preparationTime;
+}
