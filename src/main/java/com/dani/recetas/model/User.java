3a1da234b8203c75eaf59ac07cc5f81a573766e0
Daniel Juan <dannijl88web@gmail.com>
2026-03-05 21:53:55 +0100

    Add User entity and configure MySQL connection

---
+++ src/main/java/com/dani/recetas/model/User.java
@@ -0,0 +1,28 @@
+package com.dani.recetas.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+}
