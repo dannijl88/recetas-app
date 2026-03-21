39da6418cef7bd9b06aa68170e51d20f27117b20
Daniel Juan <dannijl88web@gmail.com>
2026-03-21 20:10:28 +0100

    Add React frontend with TypesScript and Tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+        <h1 className='text-3xl font-bold text-red-500'>Recetas App</h1>
+    </div>
+  )
+}
+
+export default App
