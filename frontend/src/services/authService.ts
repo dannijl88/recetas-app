a392b67bd1615c11dd2de6100fca18d9797b67d7
Daniel Juan <dannijl88web@gmail.com>
2026-03-23 10:57:36 +0100

    Add auth service

---
+++ frontend/src/services/authService.ts
@@ -0,0 +1,11 @@
+import api from './api';
+
+export const login = async (username: string, password: string) => {
+    const response = await api.post("auth/login", {username, password});
+    const token = response.headers['authorization'];
+    return token;
+}
+
+export const register = async (username: string, email: string, password: string) => {
+    await api.post("/auth/register", {username, email, password});
+}
